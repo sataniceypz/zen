53a2e87f724439be59504ee69fb365ee697ae735
Eypz <105191630+sataniceypz@users.noreply.github.com>
2025-03-20 02:15:29 +0530

    Update converter.js

--- plugins/converter.js
+++ plugins/converter.js
@@ -1,40 +1,6 @@
 const config = require("../config");
 const { command, isPrivate, getJson, sleep, tiny, AddMp3Meta, getBuffer, toAudio, styletext, listall } = require("../lib/");
 const { Image } = require("node-webpmux");
-/* Copyright (C) 2024 Louis-X0.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-Louis-X0 - Zeta-X0
-*/
-
-/*command(
-    {
-        pattern: "attp",
-        fromMe: isPrivate,
-        desc: "text to animation sticker",
-        type: "converter",
-    },
-    async (message, match) => {
-    	
-    	
-   if (!match)return message.reply(`*_Need a text_*`)
-let buff = await getBuffer(`https://vihangayt.me/maker/text2gif?q=${match}`);
-    await message.sendMessage(
-      buff,
-      { packname: config.STICKER_DATA.split(";")[0], author: config.STICKER_DATA.split(";")[1] },
-      "sticker"
-    );
-});
-*/
-
-
-/* Copyright (C) 2024 Louis-X0.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-Louis-X0 - Zeta-XD
-*/
-
-
 command(
   {
     pattern: "fancy",
@@ -59,12 +25,6 @@ command(
 );
 
 
-/* Copyright (C) 2024 Louis-XD.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-Louis-XD - Zeta-XD
-*/
-
 command(
   {
     pattern: "sticker",
@@ -83,12 +43,6 @@ command(
     );
   }
 );
-
-/* Copyright (C) 2022 X-Electra.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-X-Asena - X-Electra
-*/
 
 command(
   {
