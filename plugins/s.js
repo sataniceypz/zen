82dc2ee928e21057f8768cc54ff760031369071a
Eypz <105191630+sataniceypz@users.noreply.github.com>
2025-03-20 02:17:05 +0530

    Add files via upload

---
+++ plugins/s.js
@@ -0,0 +1,43 @@
+const { command , isPrivate , getBuffer, getJson, isUrl } = require("../lib");
+const fetch = require("node-fetch");
+const ytdl = require("ytdl-core")
+const { CAPTION } = require("../config");
+const axios = require("axios");
+const X = require("../config");
+
+
+command(
+    {
+        pattern: "s",
+        fromMe: isPrivate,
+        desc: "Song Downloader",
+        type: "downloader",
+    },
+    async (message, match, client) => {
+  if (!match) {
+    return await message.reply("Please provide a search query or YouTube URL.");
+  }
+
+  try {
+    const { videos } = await yts(match);
+    const firstVideo = videos[0];
+    const url = firstVideo.url;
+    const api = `https://api.siputzx.my.id/api/d/ytmp4?url=${url}`;
+
+    const response = await fetch(api);
+    const result = await response.json();
+    const data = result.data;
+    const dl = data.dl;
+    const title = data.title;
+
+    await message.reply(`_Downloading ${title}_`);
+    await client.sendMessage(message.jid, {
+      audio: { url: dl },
+      caption: title,
+      mimetype: "audio/mpeg",
+    }, { quoted: message.data });
+  } catch (error) {
+    console.error("Error:", error);
+    await message.reply("An error occurred while processing your request. Please try again later.");
+  }
+});
