c2ab01e4344e41d3bbf4b43bac643340b1da3be0
Rizwaan!!!! <lynxrepo677@gmail.com>
2025-03-21 10:43:02 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -9,19 +9,19 @@ command(
   async (message, match, m, client) => {
   const zeta = {
   'contacts': {
-    'displayName': "𝐄𝐘𝐏𝐙",
+    'displayName': "𝐋𝐲𝐧𝐱",
     'contacts': [{
-      'vcard': "BEGIN:VCARD\nVERSION:3.0\nFN:𝐄𝐘𝐏𝐙\nORG:Eypz\nTEL;type=CELL;type=VOICE;waid=917994489493:917994489493\nEND:VCARD"
+      'vcard': "BEGIN:VCARD\nVERSION:3.0\nFN:𝐋𝐲𝐧𝐱\nORG:Lynx\nTEL;type=CELL;type=VOICE;waid=918138898059:918138898059\nEND:VCARD"
     }]
   },
   'contextInfo': {
     'externalAdReply': {
-      'title': "𝗜𝗭𝗨𝗠𝗜 𝗫𝗗🧚‍♂️",
-      'body': "𝐄𝐘𝐏𝐙",
+      'title': "𝐙𝐞𝐧 𝐗𝐝🧚‍♂️",
+      'body': "𝐋𝐲𝐧𝐱",
       'thumbnailUrl': "https://i.imgur.com/Uc0uIkO.jpeg",
       'mediaType': 0x1,
-      'mediaUrl': "http://wa.me/917994489493",
-      'sourceUrl': "http://wa.me/917994489493",
+      'mediaUrl': "http://wa.me/918138898059",
+      'sourceUrl': "http://wa.me/918138898059",
       'showAdAttribution': false
     }
   }
